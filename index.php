ba61e024452b42771236fa68b3fe7c1069f5260c
saitoyoshi <mitti180@gmail.com>
2023-06-01 10:24:13 +0900

    レイアウトファイルを追加

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/custom.css">
-    <title>ホーム</title>
-</head>
-<body>
-    it works
-</body>
-</html>
+<?php
+
+$title = 'ホーム';
+$content =  __DIR__ . '/views/index.php';
+include __DIR__ . '/views/layout.php';
