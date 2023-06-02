511add038a7a1a56c0090abe3ce22b181591002d
saitoyoshi <mitti180@gmail.com>
2023-06-02 10:12:05 +0900

    ハンバーガーボタンを上部にレイアウト

--- views/index.php
+++ views/index.php
@@ -4,7 +4,7 @@
             <a href="#" class="navbar-brand"><picture><source media="(min-width: 576px)" srcset="../img/logo_sm.png" class="img-my-fluid"><img src="../img/logo_sm.png" alt="ロゴ" srcset="" class="img-my-fluid mb-3" style=""></picture><br>
             一般社団法人<br>
             <span class="ms-5 ps-4">こ・こいるTOYOMA</span></a>
-            <button class="navbar-toggler mb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
+            <button class="navbar-toggler mb-5 pb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
             <div class="navbar-collapse collapse" id="nav-bar-content">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a href="#" class="nav-link">ホーム</a></li>
