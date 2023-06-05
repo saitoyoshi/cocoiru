8b189ab1b07e252182a0efdfe62b7cc2b1b9a0bd
saitoyoshi <mitti180@gmail.com>
2023-06-06 08:20:13 +0900

    最小ブレイクポイントのときのみパステル画像の上にパティングを設定

--- views/index.php
+++ views/index.php
@@ -2,34 +2,37 @@
     <nav class="navbar navbar-expand-sm bg-white navbar-light pb-0">
         <div class="container-fluid my-navbar">
             <div class="navbar-brand d-flex flex-column flex-sm-row w-75"><a href="index.php" class="text-dark text-decoration-none">
-                <div class="col-sm-6">
-                <picture><source media="(min-width: 576px)" srcset="../img/logo_sm.png" class="img-my-fluid"><img src="../img/logo_sm.png" alt="ロゴ" srcset="" class="img-my-fluid mb-3" style=""></picture>
-                </div>
-                <div class="col-sm-6  mt-sm-3 mt-md-5 ms-md-5">
-            <h2 class="">一般社団法人</h2>
-            <h2 class="ms-5 ps-4">こ・こいるTOYOMA</h2></a></div>
-            </div>
-            <button class="navbar-toggler mb-5 pb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
-            <div class="navbar-collapse collapse vw-100 navbar-content-color justify-content-sm-end pe-3" id="nav-bar-content">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a href="#" class="nav-link">ホーム</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">私たちの活動</a></li>
-                    <li class="nav-item"><a href="#contact" class="nav-link">お問い合わせ</a></li>
-                    <li class="nav-item"><a href="../landlords/ooya1.php" class="nav-link">ご協力いただく大家さんを探しています</a></li>
-                </ul>
-            </div>
+                    <div class="col-sm-6">
+                        <picture>
+                            <source media="(min-width: 576px)" srcset="../img/logo_sm.png" class="img-my-fluid"><img src="../img/logo_sm.png" alt="ロゴ" srcset="" class="img-my-fluid mb-3" style="">
+                        </picture>
+                    </div>
+                    <div class="col-sm-6  mt-sm-3 mt-md-5 ms-md-5">
+                        <h2 class="">一般社団法人</h2>
+                        <h2 class="ms-5 ps-4">こ・こいるTOYOMA</h2>
+                </a></div>
         </div>
-    </nav>
+        <button class="navbar-toggler mb-5 pb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
+        <div class="navbar-collapse collapse vw-100 navbar-content-color justify-content-sm-end pe-3" id="nav-bar-content">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link">ホーム</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">私たちの活動</a></li>
+                <li class="nav-item"><a href="#contact" class="nav-link">お問い合わせ</a></li>
+                <li class="nav-item"><a href="../landlords/ooya1.php" class="nav-link">ご協力いただく大家さんを探しています</a></li>
+            </ul>
+        </div>
 </div>
-    <div class="container-container-fluid position-relative">
+</nav>
+</div>
+<div class="container-container-fluid position-relative pt-5 pt-sm-0">
 
-        <img src="../img/pastel.jpg" alt="パステル背景" srcset="" class="pastel-img vw-100">
-        <div class="pastel-front-text-container position-absolute translate-middle top-50 start-50">
-            <h1 class="" ><span class="yellow-marker">こ・こいるTOYOMAでは</span></h1>
-            <br>
-            <h1 class="ms-5 ps-5"><span class="yellow-marker ">ご高齢者の方</span></h1>
-            <h1 class="ms-5 ps-5"><span class="yellow-marker ">障害をお持ちの方</span></h1>
-            <h1 class="ms-5 ps-5"><span class="yellow-marker ">子育て世代</span></h1>
-            <h1 class="ms-5 ps-5"><span class="yellow-marker ">保証人がいない</span></h1>
-        </div>
+    <img src="../img/pastel.jpg" alt="パステル背景" srcset="" class="pastel-img vw-100">
+    <div class="pastel-front-text-container position-absolute translate-middle top-50 start-50">
+        <h1 class=""><span class="yellow-marker">こ・こいるTOYOMAでは</span></h1>
+        <br>
+        <h1 class="ms-5 ps-5"><span class="yellow-marker ">ご高齢者の方</span></h1>
+        <h1 class="ms-5 ps-5"><span class="yellow-marker ">障害をお持ちの方</span></h1>
+        <h1 class="ms-5 ps-5"><span class="yellow-marker ">子育て世代</span></h1>
+        <h1 class="ms-5 ps-5"><span class="yellow-marker ">保証人がいない</span></h1>
     </div>
+</div>
