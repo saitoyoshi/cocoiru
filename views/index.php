51f0323e32f407035983a39cf55ed71f53455083
saitoyoshi <mitti180@gmail.com>
2023-06-05 09:10:11 +0900

    ブランドの見出しとナブバーコンテントの背景色を変更

--- views/index.php
+++ views/index.php
@@ -6,11 +6,11 @@
                 <picture><source media="(min-width: 576px)" srcset="../img/logo_sm.png" class="img-my-fluid"><img src="../img/logo_sm.png" alt="ロゴ" srcset="" class="img-my-fluid mb-3" style=""></picture>
                 </div>
                 <div class="col-sm-6  mt-sm-3 mt-md-5 ms-md-5">
-            一般社団法人<br>
-            <span class="ms-5 ps-4">こ・こいるTOYOMA</span></a></div>
+            <span class="my-ft-big">一般社団法人</span><br>
+            <span class="ms-5 ps-4 my-ft-big">こ・こいるTOYOMA</span></a></div>
             </div>
             <button class="navbar-toggler mb-5 pb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
-            <div class="navbar-collapse collapse" id="nav-bar-content">
+            <div class="navbar-collapse collapse vw-100 navbar-content-color justify-content-sm-end pe-3" id="nav-bar-content">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a href="#" class="nav-link">ホーム</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">私たちの活動</a></li>
