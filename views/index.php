f658e4fa379c3c946178cf60ce43aa82922f464e
saitoyoshi <mitti180@gmail.com>
2023-06-07 05:33:36 +0900

    横幅が揃わない問題を解決

--- views/index.php
+++ views/index.php
@@ -4,7 +4,8 @@
             <div class="navbar-brand d-flex flex-column flex-sm-row w-75"><a href="index.php" class="text-dark text-decoration-none">
                     <div class="col-sm-6">
                         <picture>
-                            <source media="(min-width: 576px)" srcset="../img/logo_sm.png" class="img-my-fluid"><img src="../img/logo_sm.png" alt="ロゴ" srcset="" class="img-my-fluid mb-3" style="">
+                            <source media="(min-width: 576px)" srcset="../img/logo_sm.png"  class="img-my-fluid" >
+                            <img src="../img/logo_sm.png" alt="ロゴ" width="185" height="169" class="img-my-fluid mb-3">
                         </picture>
                     </div>
                     <div class="col-sm-6  mt-sm-3 mt-md-5 ms-md-5">
@@ -12,8 +13,8 @@
                         <h4 class="ms-5 ps-4 fs-2">こ・こいるTOYOMA</h4>
                 </a></div>
         </div>
-        <button class="navbar-toggler mb-5 pb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
-        <div class="navbar-collapse collapse vw-100 navbar-content-color justify-content-sm-end pe-3" id="nav-bar-content">
+        <button class="navbar-toggler mb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
+        <div class="navbar-collapse collapse max-width-wrapper navbar-content-color justify-content-sm-end " id="nav-bar-content">
             <ul class="navbar-nav">
                 <li class="nav-item"><a href="#" class="nav-link">ホーム</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">私たちの活動</a></li>
@@ -24,9 +25,9 @@
 </div>
 </nav>
 </div>
-<div class="container-container-fluid position-relative pt-5 pt-sm-0">
+<div class="container-fluid position-relative pt-5 pt-sm-0">
 
-    <img src="../img/pastel.jpg" alt="パステル背景" srcset="" class="pastel-img vw-100">
+    <img src="../img/pastel.jpg" alt="パステル背景" width="1929" height="1071" class="pastel-img max-width-wrapper">
     <div class="pastel-front-text-container position-absolute pt-5 pt-sm-0">
         <h1 class="fs-1 mb-5"><span class="yellow-marker">こ・こいるTOYOMAでは</span></h1>
         <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">ご高齢者の方</span></h3>
@@ -35,8 +36,14 @@
         <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">保証人がいない</span></h3>
     </div>
 </div>
-<div class="container-fluid pastel-below-texts text-center py-4">
+<div class="container-fluid pastel-below-texts text-center py-4 mw-100">
 <h5 class="fs-3 mb-4">などの理由で住まいの確保にお困りの方に
 </h5>
 <h5 class="fs-3">安心して暮らせるサービスをご提供します！</h5>
+</div>
+<div class="container-fluid position-relative">
+    <img src="../img/green.jpg" alt="森背景" width="980" height="674" class="max-width-wrapper img-fluid">
+    <div class="container forest-front-text-container position-absolute top-50 start-50 translate-middle py-4">
+        <p class="text-bg-danger">aaaaa</p>
+    </div>
 </div>
