40b7dbc7d9a4457f3df7d8cf94664139871327f9
saitoyoshi <mitti180@gmail.com>
2023-06-08 04:54:28 +0900

    ナブバー完成

--- views/index.php
+++ views/index.php
@@ -1,30 +1,19 @@
-<div class="container pt-4">
-    <nav class="navbar navbar-expand-sm bg-white navbar-light pb-0">
-        <div class="container-fluid my-navbar">
-            <div class="navbar-brand d-flex flex-column flex-sm-row w-75"><a href="index.php" class="text-dark text-decoration-none">
-                    <div class="col-sm-6">
-                        <picture>
-                            <source media="(min-width: 576px)" srcset="../img/logo_sm.png" class="img-my-fluid">
-                            <img src="../img/logo_sm.png" alt="ロゴ" width="185" height="169" class="img-my-fluid mb-3">
-                        </picture>
-                    </div>
-                    <div class="col-sm-6  mt-sm-3 mt-md-5 ms-md-5">
-                        <h4 class="fs-2">一般社団法人</h4>
-                        <h4 class="ms-5 ps-4 fs-2">こ・こいるTOYOMA</h4>
-                </a></div>
-        </div>
-        <button class="navbar-toggler mb-5" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
-        <div class="navbar-collapse collapse max-width-wrapper navbar-content-color justify-content-sm-end " id="nav-bar-content">
-            <ul class="navbar-nav">
-                <li class="nav-item fs-4"><a href="#" class="nav-link">ホーム</a></li>
-                <li class="nav-item fs-4"><a href="#" class="nav-link">私たちの活動</a></li>
-                <li class="nav-item fs-4"><a href="#contact" class="nav-link">お問い合わせ</a></li>
-                <li class="nav-item fs-4"><a href="../landlords/ooya1.php" class="nav-link pe-4">ご協力いただく大家さんを探しています</a></li>
-            </ul>
-        </div>
-</div>
-</nav>
-</div>
+<div class="wrapper">
+        <nav class="pt-0 navbar navbar-expand-lg bg-white navbar-light pb-0">
+            <div class="container-fluid navbar-content-color">
+                <a class="navbar-brand fs-4" href="index.php"><img class="logo me-3" src="../img/logo_sm.png" alt="ロゴ" srcset="" width="185" height="169">一般社団法人<span class="ms-3">こ・こいるTOYOMA</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
+            <div class="navbar-collapse collapse " id="nav-bar-content">
+                <ul class="navbar-nav ms-auto justify-content-end">
+                    <li class="nav-item fs-5"><a href="#" class="nav-link">ホーム</a></li>
+                    <li class="nav-item fs-5"><a href="#" class="nav-link">私たちの活動</a></li>
+                    <li class="nav-item fs-5"><a href="#contact" class="nav-link">お問い合わせ</a></li>
+                    <li class="nav-item fs-5"><a href="../landlords/ooya1.php" class="nav-link pe-4">ご協力いただく大家さんを探しています</a></li>
+                </ul>
+            </div>
+    </div>
+    </div>
+    </nav>
 <div class="my-container-fluid position-relative pt-5 pt-sm-0">
 
     <img src="../img/pastel.jpg" alt="パステル背景" width="1929" height="1071" class="pastel-img max-width-wrapper">
@@ -54,37 +43,23 @@
     </div>
 </div>
 <div class="my-container-fluid">
-    <div class="row">
-        <img src="../img/my-p.png" alt="人々" srcset="" class="img-fluid col-md-6" width="632" height="601">
+    <div class="row justify-content-center">
+        <img src="../img/my-p.png" alt="人々" srcset="" class="img-fluid col-md-6 max-width-wrapper" width="632" height="601">
         <div class="col-md-6 bg-white py-4 px-3 d-md-flex align-items-md-center">
             <div class="container p-4" style="background-color: #ffeee5;">
-            <h4 class="fs-3 mb-3">サポートについて</h4>
-            <h1 class="fs-2 mb-3">入居、入居後のサポート</h1>
-            <ul>
-                <li class="p-font">不動産店舗と調整</li>
-                <li class="p-font">案内物件への見学同行</li>
-                <li class="p-font">契約手続きのお手伝い</li>
-                <li class="p-font">入居準備の支援</li>
-                <li class="p-font">見守りサービス</li>
-                <li class="p-font">緊急連絡先対応</li>
-                <li class="p-font">生活相談・トラブル対応</li>
-            </ul>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                <h4 class="fs-3 mb-3">サポートについて</h4>
+                <h1 class="fs-2 mb-3">入居、入居後のサポート</h1>
+                <ul>
+                    <li class="p-font">不動産店舗と調整</li>
+                    <li class="p-font">案内物件への見学同行</li>
+                    <li class="p-font">契約手続きのお手伝い</li>
+                    <li class="p-font">入居準備の支援</li>
+                    <li class="p-font">見守りサービス</li>
+                    <li class="p-font">緊急連絡先対応</li>
+                    <li class="p-font">生活相談・トラブル対応</li>
+                </ul>
             </div>
         </div>
     </div>
+</div>
 </div>
