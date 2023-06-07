18ed6f16335421c658dba8dea701730644a47422
saitoyoshi <mitti180@gmail.com>
2023-06-08 07:25:01 +0900

    不要なCSSクラスを削除

--- views/index.php
+++ views/index.php
@@ -1,7 +1,7 @@
 <div class="wrapper">
-        <nav class="pt-0 navbar navbar-expand-lg bg-white navbar-light pb-0">
-            <div class="container-fluid navbar-content-color">
-                <a class="navbar-brand fs-4" href="index.php"><img class="logo me-3" src="../img/logo_sm.png" alt="ロゴ" srcset="" width="185" height="169">一般社団法人<span class="ms-3">こ・こいるTOYOMA</span></a>
+    <nav class="navbar navbar-expand-lg bg-white navbar-light py-0">
+        <div class="container-fluid navbar-content-color">
+            <a class="navbar-brand fs-4" href="index.php"><img class="logo me-3" src="../img/logo_sm.png" alt="ロゴ" srcset="" width="185" height="169">一般社団法人<span class="ms-3">こ・こいるTOYOMA</span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-bar-content"><span class="navbar-toggler-icon"></span></button>
             <div class="navbar-collapse collapse " id="nav-bar-content">
                 <ul class="navbar-nav ms-auto justify-content-end">
@@ -11,55 +11,55 @@
                     <li class="nav-item fs-5"><a href="../landlords/ooya1.php" class="nav-link pe-4">ご協力いただく大家さんを探しています</a></li>
                 </ul>
             </div>
-    </div>
-    </div>
-    </nav>
-<div class="my-container-fluid position-relative">
+        </div>
 
-    <img src="../img/pastel.jpg" alt="パステル背景" width="1929" height="1071" class="pastel-img max-width-wrapper">
-    <div class="pastel-front-text-container position-absolute pt-5 pt-sm-0">
-        <h1 class="fs-1 mb-5"><span class="yellow-marker">こ・こいるTOYOMAでは</span></h1>
-        <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">ご高齢者の方</span></h3>
-        <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">障害をお持ちの方</span></h3>
-        <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">子育て世代</span></h3>
-        <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">保証人がいない</span></h3>
+    </nav>
+    <div class=" position-relative">
+
+        <img src="../img/pastel.jpg" alt="パステル背景" width="1929" height="1071" class="pastel-img max-width-wrapper">
+        <div class="pastel-front-text-container position-absolute pt-5 pt-sm-0">
+            <h1 class="fs-1 mb-5"><span class="yellow-marker">こ・こいるTOYOMAでは</span></h1>
+            <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">ご高齢者の方</span></h3>
+            <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">障害をお持ちの方</span></h3>
+            <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">子育て世代</span></h3>
+            <h3 class="ms-5 ps-5 fs-1"><span class="yellow-marker ">保証人がいない</span></h3>
+        </div>
     </div>
-</div>
-<div class="my-container-fluid pastel-below-texts text-center py-4 mw-100">
-    <h5 class="fs-3 mb-4">などの理由で住まいの確保にお困りの方に
-    </h5>
-    <h5 class="fs-3">安心して暮らせるサービスをご提供します！</h5>
-</div>
-<div class="my-container-fluid position-relative py-4 px-3" style="background-image: url(../img/green.jpg);">
-    <div class="bg-white container text-center p-4 consultation-max-width">
-        <h1 class="fs-1 mb-4" style="color:#fab30b;">相談費用は一切かかりません</h1>
-        <p class="p-font">賃貸住宅のご紹介</p>
-        <p class="p-font">入居のサポート</p>
-        <p class="p-font">シェアハウスのご紹介</p>
-        <p class="p-font mb-4">グループホームのご紹介</p>
-        <p class="p-font pt-3">本人様の状況に応じた住まい方を</p>
-        <p class="p-font mb-4 pb-3">ご提案、ご紹介させていただきます</p>
-        <a href="#contact" class="btn btn-primary btn-lg text-dark" style="background-color: #c9c8f8;border-color: #c9c8f8;">お問い合わせ</a>
+    <div class=" pastel-below-texts text-center py-4 mw-100">
+        <h5 class="fs-3 mb-4">などの理由で住まいの確保にお困りの方に
+        </h5>
+        <h5 class="fs-3">安心して暮らせるサービスをご提供します！</h5>
     </div>
-</div>
-<div class="my-container-fluid">
-    <div class="row justify-content-center">
-        <img src="../img/my-p.png" alt="人々" srcset="" class="img-fluid col-md-6 max-width-wrapper" width="632" height="601">
-        <div class="col-md-6 bg-white py-4 px-3 d-md-flex align-items-md-center">
-            <div class="container p-4" style="background-color: #ffeee5;">
-                <h4 class="fs-3 mb-3">サポートについて</h4>
-                <h1 class="fs-2 mb-3">入居、入居後のサポート</h1>
-                <ul>
-                    <li class="p-font">不動産店舗と調整</li>
-                    <li class="p-font">案内物件への見学同行</li>
-                    <li class="p-font">契約手続きのお手伝い</li>
-                    <li class="p-font">入居準備の支援</li>
-                    <li class="p-font">見守りサービス</li>
-                    <li class="p-font">緊急連絡先対応</li>
-                    <li class="p-font">生活相談・トラブル対応</li>
-                </ul>
+    <div class=" position-relative py-4 px-3" style="background-image: url(../img/green.jpg);">
+        <div class="bg-white container text-center p-4 consultation-max-width">
+            <h1 class="fs-1 mb-4" style="color:#fab30b;">相談費用は一切かかりません</h1>
+            <p class="p-font">賃貸住宅のご紹介</p>
+            <p class="p-font">入居のサポート</p>
+            <p class="p-font">シェアハウスのご紹介</p>
+            <p class="p-font mb-4">グループホームのご紹介</p>
+            <p class="p-font pt-3">本人様の状況に応じた住まい方を</p>
+            <p class="p-font mb-4 pb-3">ご提案、ご紹介させていただきます</p>
+            <a href="#contact" class="btn btn-primary btn-lg text-dark" style="background-color: #c9c8f8;border-color: #c9c8f8;">お問い合わせ</a>
+        </div>
+    </div>
+    <div class="">
+        <div class="row justify-content-center">
+            <img src="../img/my-p.png" alt="人々" srcset="" class="img-fluid col-md-6 max-width-wrapper" width="632" height="601">
+            <div class="col-md-6 bg-white py-4 px-3 d-md-flex align-items-md-center">
+                <div class="container p-4" style="background-color: #ffeee5;">
+                    <h4 class="fs-3 mb-3">サポートについて</h4>
+                    <h1 class="fs-2 mb-3">入居、入居後のサポート</h1>
+                    <ul>
+                        <li class="p-font">不動産店舗と調整</li>
+                        <li class="p-font">案内物件への見学同行</li>
+                        <li class="p-font">契約手続きのお手伝い</li>
+                        <li class="p-font">入居準備の支援</li>
+                        <li class="p-font">見守りサービス</li>
+                        <li class="p-font">緊急連絡先対応</li>
+                        <li class="p-font">生活相談・トラブル対応</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </div>
