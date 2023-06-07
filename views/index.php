1bc2ee1b6541a318963dadcdfb270ce0b4722763
saitoyoshi <mitti180@gmail.com>
2023-06-07 10:49:13 +0900

    相談費用は～の部分のマークアップ作成

--- views/index.php
+++ views/index.php
@@ -4,7 +4,7 @@
             <div class="navbar-brand d-flex flex-column flex-sm-row w-75"><a href="index.php" class="text-dark text-decoration-none">
                     <div class="col-sm-6">
                         <picture>
-                            <source media="(min-width: 576px)" srcset="../img/logo_sm.png"  class="img-my-fluid" >
+                            <source media="(min-width: 576px)" srcset="../img/logo_sm.png" class="img-my-fluid">
                             <img src="../img/logo_sm.png" alt="ロゴ" width="185" height="169" class="img-my-fluid mb-3">
                         </picture>
                     </div>
@@ -37,13 +37,21 @@
     </div>
 </div>
 <div class="my-container-fluid pastel-below-texts text-center py-4 mw-100">
-<h5 class="fs-3 mb-4">などの理由で住まいの確保にお困りの方に
-</h5>
-<h5 class="fs-3">安心して暮らせるサービスをご提供します！</h5>
+    <h5 class="fs-3 mb-4">などの理由で住まいの確保にお困りの方に
+    </h5>
+    <h5 class="fs-3">安心して暮らせるサービスをご提供します！</h5>
 </div>
-<div class="my-container-fluid position-relative">
-    <img src="../img/green.jpg" alt="森背景" width="980" height="674" class="max-width-wrapper img-fluid">
-    <div class="container forest-front-text-container position-absolute top-50 start-50 translate-middle py-4">
-        <p class="text-bg-danger">aaaaa</p>
-    </div>
+<div class="my-container-fluid position-relative py-4 px-3" style="background-image: url(../img/green.jpg);">
+<div class="bg-white container text-center p-3">
+    <h1 class="fs-1">相談費用は一切かかりません</h1>
+    <!-- p raleway -->
+    <p>賃貸住宅のご紹介</p>
+    <p>入居のサポート</p>
+    <p>シェアハウスのご紹介</p>
+    <p>グループホームのご紹介</p>
+    <p>本人様の状況に応じた住まい方を</p>
+    <p>ご提案、ご紹介させていただきます</p>
+    <button type="button">お問い合わせ</button>
+</div>
+    <!-- <img src="../img/green.jpg" alt="森背景" width="980" height="674" class="max-width-wrapper img-fluid"> -->
 </div>
