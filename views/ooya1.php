5e359c0f6e0b49bc631628cd8b5c9f019d42ade1
saitoyoshi <mitti180@gmail.com>
2023-06-19 09:02:27 +0900

    大家さんページ完成いっこまえのはコミット間違い

--- views/ooya1.php
+++ views/ooya1.php
@@ -1,1 +1,32 @@
-<p>aaa</p>
+<main>
+    <div class="container text-center pb-4 pt-5">
+
+
+<h2 class="page-header mb-5">ご協力いただく<br>
+​大家さんを探しています。</h2>
+<p class="p-font mb-5">空き家・空き室の活用をお考えの大家さんへ</p>
+<p class="p-font please-rent-text mb-5">住まい探しに、お困りの方に<br>
+​お部屋をお貸頂けませんか？</p>
+<h3 class="canbe-support-text mb-5">セーフティネット住宅<br>（住居確保要配慮者専門賃貸住宅）<br>にご登録いたくだくと</h3>
+<h3 class="canbe-support-text">住宅のバリアフリー化や間取り<br>改修工事として補助金の<br>サポートが受けられます。</h3>
+</div>
+<img src="../img/working.webp"  class="working-img" alt="作業員" srcset="">
+<div class="container py-5">
+    <ul>
+        <li class="mb-4">住宅確保要配慮者（低所得者、被災者、高齢者、障がい者、子どもを養育されている方など）の入居を拒まない賃貸住宅として、都道府県にご登録いただけます。</li>
+        <li class="mb-4">登録した空き家、空き室は、国が管理する専用ホームページに掲載され、広く皆さんに知ってもらえます。</li>
+        <li class="mb-4">大家さんは、入居舎受け入れにあたり、必要な改修工事の補助を受けることができます。（補助を受けるには、一定の要件があります。）</li>
+        <li>高齢者や障がい者方の入居に対しては、見守り等の住居支　援サポートが入ります。</li>
+    </ul>
+</div>
+<div class="container wrapper-contact-over-flow-img">
+    <img src="../img/please-contact.webp" alt="" srcset="">
+    <div class="contact-overflow-img-text">
+        <p class="ps-3">空いている戸建て住宅、アパートを<br>
+お持ちの方、こ・こいるTOYAMAまで<br>
+​お問い合わせください</p>
+    </div>
+</div>
+<div class="text-center mt-5 mt-sm-0">
+<a href="index.php#contact" class="btn btn-lg contact-button text-dark">お問い合わせ</a>
+</div>
